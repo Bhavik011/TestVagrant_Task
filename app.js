f96a359853ee8e3f3bfb4d4542c0d03feefce4d7
Bhavik011 <bhavikjain0110@gmail.com>
2022-05-26 12:22:04 +0530

    added try catch block for error detection and prompt for user input

--- app.js
+++ app.js
@@ -1,14 +1,30 @@
-import {solution,average} from './utility.js';
+import {
+    solution,
+    average
+} from './utility.js';
+import promptSync from 'prompt-sync';
 
-const outcome ='W'  //'W','L
-const count=2; 
+const prompt = promptSync();
+try {
+    const outcome = prompt("Enter the outcome as 'W','L' ");
+    if (outcome != 'W' && outcome != 'L') throw "Invalid outcome. Please Enter 'W' or 'L'";
+    let count = prompt("Enter count of consecutive wins or losses:");
+    count = parseInt(count);
+    if (typeof count != 'number') throw "Invalid count. Enter a number";
 
-const teams = solution(outcome, count);
+    const teams = solution(outcome, count);
 
-if (teams.length == 0) {
-    console.log('No teams found');
-} else
-    console.log(teams);
+    if (teams.length == 0) {
+        console.log('No teams found');
+    } else
+        console.log(teams);
 
-    const averagePoints=average();
+    const averagePoints = average();
     console.log("average points of teams are: " + averagePoints);
+
+} catch (e) {
+    console.log(e);
+}
+
+//  outcome = 'W' or 'L
+//  count=2; 
