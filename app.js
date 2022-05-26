1fa2d83ec0a59e534825b639231722b7ca378d0b
Bhavik011 <bhavikjain0110@gmail.com>
2022-05-26 10:44:06 +0530

    added functionality to generalize solution for n consective wins/losses

--- app.js
+++ app.js
@@ -1,14 +1,22 @@
-import {data} from './data.js';
-const teams=[];
+import {
+    data
+} from './data.js';
+const teams = [];
 
-data.forEach((team)=>{
-    var count=0;
-    team.Results.forEach((result)=>{
-        result=='L'?count++:count=0;
-        if(count==2){
-        teams.push(team);
-        return;
-        }
+const solution = (outcome, repetition) => {
+    data.forEach((team) => {
+        var count = 0;
+        team.Results.forEach((result) => {
+            result == outcome ? count++ : count = 0;
+            if (count == repetition) {
+                teams.push(team);
+                return;
+            }
+        })
     })
-})
-console.log(teams);
+}
+solution('W', 2);
+if (teams.length == 0) {
+    console.log('No teams found');
+} else
+    console.log(teams);
