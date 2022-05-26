1a7cbbd73bffa6c87bcdb2e1ec68b64608d8a82e
Bhavik011 <bhavikjain0110@gmail.com>
2022-05-26 10:36:38 +0530

    added functionality to give teams with two consecutive losses

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
 import {data} from './data.js';
+const teams=[];
+
+data.forEach((team)=>{
+    var count=0;
+    team.Results.forEach((result)=>{
+        result=='L'?count++:count=0;
+        if(count==2){
+        teams.push(team);
+        return;
+        }
+    })
+})
+console.log(teams);
